3047d9902f5cbe05865b42f00183627cebe8f0a4
JMStewart00 <jmstewart00@gmail.com>
2022-03-15 19:27:36 -0400

    Add spicy level to items

--- menu_items/models.py
+++ menu_items/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
@@ -14,6 +15,13 @@ class Item(models.Model):
     title = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField(null=True, blank=True)
+    spicy_level = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ]
+    )
 
     def __str__(self):
         return self.title
